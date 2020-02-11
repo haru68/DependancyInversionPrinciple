1d9109ed36e157b8aceb0f0af18c44ed7b2c8c59
Kevin Sartori <kevin.bouty@hotmail.fr>
2020-02-11 15:38:15 +0100

    First commit

---
+++ Interface1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using System.Linq;
+using System.IO;
+
+namespace PrincipeInversionDependance
+{
+    public interface Interface1
+    {
+        public bool Exited { get; set; }
+
+        public abstract string Prompt();
+        public abstract Command PromptCommand();
+        public abstract void ExecuteCommand(Command command);
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.IO;
+
+namespace PrincipeInversionDependance
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Interface1 terminal = new Terminal();
+            while (!terminal.Exited)
+            {
+                Command command = terminal.PromptCommand();
+                terminal.ExecuteCommand(command);
+            }
+        }
+    }
+}
+
+
+
